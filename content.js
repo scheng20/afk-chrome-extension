897864040bcd7b9ed36cff693b2afd69ed33bbaf
scheng20 <chengsheena25@gmail.com>
2020-06-29 20:18:13 -0700

    cleaned up content.js code

--- content.js
+++ content.js
@@ -1,62 +1,25 @@
-/*
-document.addEventListener("click", function() {
-    console.log("Hello World")
-    window.scrollBy(0, 100);
 
-}, false);
-*/
+// Web-speech API adapted from https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API/Using_the_Web_Speech_API
 
-//import { getHeapSpaceStatistics } from "v8";
-
-// adapted from https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API/Using_the_Web_Speech_API
-
-// in progress
+// Web-speech API setup
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
-console.log("hellow world");
 
 var cmds = ['up' , 'down'];
 var grammar = '#JSGF V1.0; grammar cmds; public <cmds> = ' + cmds.join(' | ') + ' ;'
 
 var recognition = new SpeechRecognition();
-recognition.continuous = true;
 var speechRecognitionList = new SpeechGrammarList();
 
-speechRecognitionList.addFromString(grammar, 1);
-
+recognition.continuous = true;
 recognition.grammars = speechRecognitionList;
 //recognition.continuous = false;
 recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
-//var diagnostic = document.querySelector('.output');
-//var bg = document.querySelector('html');
-
-
-// cmds.forEach(function(c, index){
-//   console.log(c, index);
-//   window.scrollBy(0, 1000);
-// });
-
-// Getting text via click:
-/*
-document.addEventListener("click", function() {
-
-    console.log("SCRAPING HTML:")
-
-    // Rough estimate of where the bottom of the page is
-    let botX = document.documentElement.clientWidth - 50;
-    let botY = document.documentElement.clientHeight - 50;
-
-    let elem = document.elementFromPoint(botX, botY);
-
-    console.log(elem.tagName);
-    console.log(elem.innerHTML);
-
-}, false);
-*/
+speechRecognitionList.addFromString(grammar, 1);
 
 var lastwords;
 
@@ -92,59 +55,58 @@ recognition.onresult = function(event) {
     var last = event.results.length - 1;
     var cmd = event.results[last][0].transcript.trim();
 
-    console.log('Result received: ' + cmd + '.');
+    console.log('Input recieved: ' + cmd);
+    console.log('Confidence: ' + (event.results[0][0].confidence) * 100 + "%");
 
-    if (cmd === 'go up') {
+    switch (cmd) {
+        case "go up":
+             window.scrollBy(0, -500);
+            break;
+        case "go down":
+            window.scrollBy(0, 500);
+            break;
 
-        //console.log('upppp');
-         window.scrollBy(0, -500);
+        case "open tab":
+            window.open('https://devpost.com/software/afk-bec9g1');
+            break;
 
-    } else if (cmd === 'go down') {
+        case "new tab":
+            window.open('https://devpost.com/software/afk-bec9g1');
+            break;
 
-        //console.log('downnnn');
-         window.scrollBy(0, 500);
-         
-    } else if (cmd === 'left tab') {
-        
-    } else if (cmd === 'right tab') {
-   
-    } else if (cmd === 'open tab' || cmd === 'new tab') {
-        window.open('https://www.nwhacks.io/');
+        case "close tab":
+            window.close();
+            break;
 
-    } else if (cmd === 'close tab') {
-        window.close();
+        case "go back":
+            window.history.go(-1);
+            break;
 
-    } else if (cmd === 'go back') {
-        window.history.go(-1);
+        case "go forward":
+            window.history.go(1);
+            break;
 
-    } else if (cmd === 'go forward' || cmd === 'go forwards') {
-        window.history.go(1);
+        case "refresh":
+             window.location.reload(true);
+             break;
 
-    } else if (cmd === 'refresh' || cmd === 'reload') {
-        window.location.reload(true);
+        case "reload":
+            window.location.reload(true);
+             break;
 
-    } else if (cmd === lastwords) {
+        case lastwords:
+             window.scrollBy(0, 500);
+             break;
 
-        //console.log("GOT LAST WORDS");
-        window.scrollBy(0, 500);
+        default:
+            console.log("Unrecognized command");
     }
-
-    console.log('Confidence: ' + event.results[0][0].confidence);
-    
 }
 
 recognition.onnomatch = function(event) {
-    console.log('I didnt recognise that word.');
+    console.log("Unrecognized word");
 }
 
 recognition.onerror = function(event) {
-    console.log('Error occurred in recognition: ' + event.error);
+    console.log("An error occurred in recognition: " + event.error);
 }
-
-//document.body.onclick = function() {
-//document.addEventListener("load", function(event) {
-    //recognition.start();
-//});
-
-  //console.log('Ready to receive a color command.');
-//}
